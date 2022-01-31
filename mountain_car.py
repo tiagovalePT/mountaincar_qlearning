6f07c3872625d867dd1d1aa04251e5cede48e74e
Tiago Vale <35015564+tiagovalePT@users.noreply.github.com>
2022-01-31 13:47:32 +0000

    correção ao gráfico

--- mountain_car.py
+++ mountain_car.py
@@ -1,177 +1,140 @@
-"""
-http://incompleteideas.net/MountainCar/MountainCar1.cp
-permalink: https://perma.cc/6Z2N-PFWC
-"""
-import math
 
-import numpy as np
-
+from traceback import print_tb
 import gym
-from gym import spaces
-from gym.utils import seeding
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import false
 
 
-class MountainCarEnv(gym.Env):
-    """
-    Description:
-        The agent (a car) is started at the bottom of a valley. For any given
-        state the agent may choose to accelerate to the left, right or cease
-        any acceleration.
+env = gym.make("MountainCar-v0")
 
-    Source:
-        The environment appeared first in Andrew Moore's PhD Thesis (1990).
+## 1) Variáveis globais
+DISCRETE_UNITS = 20
+NMR_EPISODES = 500
+DISCOUNT = 0.98
+EPISODE_DISPLAY = 20
+LEARNING_RATE = 0.3
+EPSILON = 0.05
+EPSILON_DECREMENTADOR = EPSILON/(NMR_EPISODES//4)
+env._max_episode_steps = 1000
 
-    Observation:
-        Type: Box(2)
-        Num    Observation               Min            Max
-        0      Car Position              -1.2           0.6
-        1      Car Velocity              -0.07          0.07
+#Environment values
+# print(env.observation_space.high)	#[0.6  1.5]
+# print(env.observation_space.low)	#[-1.2  -1.5]
+# print(env.action_space.n)			#3
 
-    Actions:
-        Type: Discrete(3)
-        Num    Action
-        0      Accelerate to the Left
-        1      Don't accelerate
-        2      Accelerate to the Right
 
-        Note: This does not affect the amount of velocity affected by the
-        gravitational pull acting on the car.
+#Q-Table de tamanho DISCRETE_UNITS*DISCRETE_BUCKETS*env.action_space.n
+#Q_TABLE = np.random.randn(DISCRETE_UNITS,DISCRETE_UNITS,env.action_space.n)
 
-    Reward:
-         Reward of 0 is awarded if the agent reached the flag (position = 0.5)
-         on top of the mountain.
-         Reward of -1 is awarded if the position of the agent is less than 0.5.
+## 2) Criação das tabelas de qualidade -> array com número de tabelas correspondente ao número de estados discretizados
+Q_TABLE = np.random.uniform(low = -1, high = 1, 
+                          size = (DISCRETE_UNITS, DISCRETE_UNITS, 
+                                  env.action_space.n))
 
-    Starting State:
-         The position of the car is assigned a uniform random value in
-         [-0.6 , -0.4].
-         The starting velocity of the car is always assigned to 0.
 
-    Episode Termination:
-         The car position is more than 0.5
-         Episode length is greater than 200
-    """
+## 3) função para calcular o estado em tempo discreto
+def discretizar_estado(estado):
+	tamanho_janela_discreta = (env.observation_space.high-env.observation_space.low)/[DISCRETE_UNITS]*len(env.observation_space.high)
+	estado_discreto = (estado-env.observation_space.low)//tamanho_janela_discreta
 
-    metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 30}
+	#integer tuple para usar mais tarde na extração dos valores da tabela Q
+	return tuple(estado_discreto.astype(int))		
 
-    def __init__(self, goal_velocity=0):
-        self.min_position = -1.2
-        self.max_position = 0.6
-        self.max_speed = 1.5
-        self.goal_position = 0.5
-        self.goal_velocity = goal_velocity
 
-        self.force = 0.001
-        self.gravity = 0.0025
+# Para estatisticas
+array_it = list()
+episodes_list = list()
+ep_rewards = []
+ep_list = []
+x_graph = 0;
 
-        self.low = np.array([self.min_position, -self.max_speed], dtype=np.float32)
-        self.high = np.array([self.max_position, self.max_speed], dtype=np.float32)
+iteration = 0
+## 4) Inicio do algoritmo
+for episode in range(NMR_EPISODES):
 
-        self.viewer = None
+	episode_reward = 0
+	done = false
 
-        self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(self.low, self.high, dtype=np.float32)
+	## 5) Discretizar o estado inicial
+	estado_discreto_atual = discretizar_estado(env.reset())
+	# print(estado_discreto_atual)
 
-        self.seed()
+	# calcular estado para renderizar o simulador
+	if episode % EPISODE_DISPLAY == 0:
+		render_state = True
+	else:
+		render_state = False
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+	## 6) Ciclo que percorre as interações dentro de cada episódio
+	while not done:
 
-    def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid" % (
-            action,
-            type(action),
-        )
+		# contar o número de iterações necessárias para atingir o objetivo
+		iteration+=1
 
-        position, velocity = self.state
-        velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
-        velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        position += velocity
-        position = np.clip(position, self.min_position, self.max_position)
-        if position == self.min_position and velocity < 0:
-            velocity = 0
+		## 7) condição para gerar uma ação random, para a ação do agente não ficar enviesada
+		if np.random.random() > EPSILON:
+			action = np.argmax(Q_TABLE[estado_discreto_atual])
+		else:
+			action = np.random.randint(0, env.action_space.n)
+		
+		## 8) outputs do ambiente em função das ações do agente
+		novo_estado, reward, done, _ = env.step(action)
 
-        done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
-        reward = -1.0
+		## 9) novo estado discreto
+		novo_estado_discreto = discretizar_estado(novo_estado)
 
-        self.state = (position, velocity)
-        return np.array(self.state, dtype=np.float32), reward, done, {}
+		# renderização do simulador
+		if render_state:
+			env.render()
 
-    def reset(self):
-        self.state = np.array([self.np_random.uniform(low=-1, high=0.2), 0])
-        return np.array(self.state, dtype=np.float32)
+		## 9) Verificação se a ação já foi tomada
+		if not done:
+			## 10) cálculo da nova matriz Q 
+			max_future_q = np.max(Q_TABLE[novo_estado_discreto])
+			current_q = Q_TABLE[estado_discreto_atual+(action,)]
+			new_q = current_q + LEARNING_RATE*(reward + DISCOUNT*max_future_q - current_q)
+			Q_TABLE[estado_discreto_atual+(action,)]=new_q
 
-    def _height(self, xs):
-        return np.sin(3 * xs) + 0.1
+		## 11) Verificar se o agente chegou à posição objetivo
+		elif novo_estado[0] >= env.goal_position:
+			# atualização da matriz Q, quando o agente chega ao objetivo
+			# if Q_TABLE[estado_discreto_atual + (action,)] == 0:
+			# 	print(episode)
 
-    def render(self, mode="human"):
-        screen_width = 600
-        screen_height = 800
+			Q_TABLE[estado_discreto_atual + (action,)] = 0
 
-        world_width = self.max_position - self.min_position
-        scale = screen_width / (world_width)
-        carwidth = 40
-        carheight = 20
+			# estatisticas para o gráfico
+			x_graph += 1
+			array_it.append(iteration)
+			episodes_list.append(x_graph)
+			iteration = 0
+		
+		## 12) atualização do esatdo discreto, para calcular a ação no inicio da nova iteração
+		estado_discreto_atual = novo_estado_discreto
+		episode_reward += reward
 
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
+	## 13) cálculo do novo epsilon
+	EPSILON = EPSILON - EPSILON_DECREMENTADOR
 
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            xs = np.linspace(self.min_position, self.max_position, 100)
-            ys = self._height(xs) + 1
-            xys = list(zip((xs - self.min_position) * scale, ys * scale))
+	ep_rewards.append(episode_reward)
+	ep_list.append(episode)
 
-            self.track = rendering.make_polyline(xys)
-            self.track.set_linewidth(4)
-            self.viewer.add_geom(self.track)
+	if not episode % EPISODE_DISPLAY:
+		avg_reward = sum(ep_rewards[-EPISODE_DISPLAY:])/len(ep_rewards[-EPISODE_DISPLAY:])	
+		print(f"Episode:{episode} avg:{avg_reward} min:{min(ep_rewards[-EPISODE_DISPLAY:])} max:{max(ep_rewards[-EPISODE_DISPLAY:])}")
 
-            clearance = 10
+env.close()
 
-            l, r, t, b = -carwidth / 2, carwidth / 2, carheight, 0
-            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            car.add_attr(rendering.Transform(translation=(0, clearance)))
-            self.cartrans = rendering.Transform()
-            car.add_attr(self.cartrans)
-            self.viewer.add_geom(car)
-            frontwheel = rendering.make_circle(carheight / 2.5)
-            frontwheel.set_color(0.5, 0.5, 0.5)
-            frontwheel.add_attr(
-                rendering.Transform(translation=(carwidth / 4, clearance))
-            )
-            frontwheel.add_attr(self.cartrans)
-            self.viewer.add_geom(frontwheel)
-            backwheel = rendering.make_circle(carheight / 2.5)
-            backwheel.add_attr(
-                rendering.Transform(translation=(-carwidth / 4, clearance))
-            )
-            backwheel.add_attr(self.cartrans)
-            backwheel.set_color(0.5, 0.5, 0.5)
-            self.viewer.add_geom(backwheel)
-            flagx = (self.goal_position - self.min_position) * scale
-            flagy1 = (self._height(self.goal_position) + 1) * scale
-            flagy2 = flagy1 + 50
-            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
-            self.viewer.add_geom(flagpole)
-            flag = rendering.FilledPolygon(
-                [(flagx, flagy2), (flagx, flagy2 - 10), (flagx + 25, flagy2 - 5)]
-            )
-            flag.set_color(0.8, 0.8, 0)
-            self.viewer.add_geom(flag)
 
-        pos = self.state[0]
-        self.cartrans.set_translation(
-            (pos - self.min_position) * scale, (self._height(pos) + 1) * scale
-        )
-        self.cartrans.set_rotation(math.cos(3 * pos))
+# plt.plot(ep_list, ep_rewards) 
+# plt.title('Mountain Car Q-Learning')
+# plt.ylabel('Average reward/Episode')
+# plt.xlabel('Episodes')
+# plt.show()
 
-        return self.viewer.render(return_rgb_array=mode == "rgb_array")
-
-    def get_keys_to_action(self):
-        # Control with left and right arrow keys.
-        return {(): 1, (276,): 0, (275,): 2, (275, 276): 1}
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
+plt.plot(np.array(episodes_list), np.array(array_it))
+plt.title('Learning Trial')
+plt.ylabel('Performance')
+plt.xlabel('Trials')
+plt.show()
